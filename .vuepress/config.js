cab73cb37006ae9564ede97b6d6c5451cfac20ad
Saint Wish <SaintWish@users.noreply.github.com>
2020-12-02 23:04:58 -0700

    vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  base: '/',
+  title: 'Dawn of Resdayn',
+  description: 'A TES3MP serious RP server.',
+
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  themeConfig: {
+    repo: 'Dawn-of-Resdayn/public-scripts',
+    docsRepo: 'Dawn-of-Resdayn/Dawn-of-Resdayn.github.io',
+    editLinks: true,
+    editLinkText: 'Help us improve this page.',
+    lastUpdated: true,
+    //logo: '/pterodactyl-flat.png',
+    nav: [
+      {
+        text: 'Documentation',
+        link: '/project/intro.md',
+      },
+      {
+        text: 'Get Help',
+        link: 'https://discord.gg/XG8r27R'
+      },
+      {
+        text: 'Forums',
+        link: 'https://resdayn.boards.net/'
+      }
+    ],
+    sidebar: {
+      '/': [
+        {
+          title: 'Information',
+          collapsable: false,
+          children: [
+            '/project/intro.md',
+            '/project/about.md',
+          ]
+        }
+      ],
+      '/guides/': [
+        {
+          title: 'Installing',
+          collapsable: false,
+          children: [
+            '/guides/installing.md',
+          ]
+        }
+      ],
+    }
+  },
+
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
