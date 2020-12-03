18390585d3a01b16c6adb14d4eddb59e3952f841
Saint Wish <SaintWish@users.noreply.github.com>
2020-12-03 06:17:02 -0700

    downgrade?

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -1,41 +1,14 @@
+/**
+ * Client app enhancement file.
+ *
+ * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
+ */
+
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
   router, // the router instance for the app
-  siteData, // site metadata
-  isServer // is this enhancement applied in server-rendering or client
+  siteData // site metadata
 }) => {
-  findVersionedPaths(siteData.themeConfig.sidebar).forEach(vp => {
-    router.addRoutes(
-      router.options.routes.map(route => {
-        if (route.path.startsWith(vp.path + vp.currentVersion)) {
-          return [
-            {
-              path: route.path.replace(vp.currentVersion, "current"),
-              redirect: route.path
-            }, {
-              path: route.path.replace(vp.currentVersion + "/", ""),
-              redirect: route.path
-            }
-          ]
-        }
-        return undefined
-      }).filter(x => x).flat()
-    )
-  })
-}
-
-function findVersionedPaths(paths) {
-  return Object.entries(paths).map(([path, children]) => {
-    return children
-      .filter(child => Array.isArray(child.versions))
-      .map(child => ({ ...child, path: pathJoin(path, child.path) }))
-  }).flat()
-}
-
-// https://stackoverflow.com/a/29855282/4430124
-function pathJoin(...parts) {
-  var separator = '/';
-  var replace = new RegExp(separator + '{1,}', 'g');
-  return parts.join(separator).replace(replace, separator);
+  // ...apply enhancements for the site.
 }
