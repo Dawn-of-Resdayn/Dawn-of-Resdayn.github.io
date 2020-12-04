4371e2ad5ffefa1bd923448f29925dd1ad7b0afb
Saint Wish <SaintWish@users.noreply.github.com>
2020-12-04 10:59:24 -0700

    Create Layout.vue

---
+++ .vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,11 @@
+<template>
+  <ParentLayout />
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+</script>
+
+<style lang="stylus">
+@require '../styles/components.styl'
+</style>
