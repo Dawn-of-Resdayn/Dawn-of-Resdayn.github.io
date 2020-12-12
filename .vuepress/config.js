ed890caf23a8a32a09eb86ffade08800ca286a8e
Saint Wish <SaintWish@users.noreply.github.com>
2020-12-12 11:57:02 -0700

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -95,7 +95,7 @@ module.exports = {
           children: [
             '/ingame/commands.md',
             '/ingame/rolls.md',
-            '/ingame/rollrpr.md',
+            '/ingame/rpr.md',
           ]
         },
       ],
